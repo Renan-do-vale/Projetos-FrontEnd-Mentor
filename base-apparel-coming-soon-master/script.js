3d245eb335e7aea1682b3fa1a14119b3890f9899
Renan do vale ferreira <Renandovaleferreira@gmail.com>
2023-07-15 16:36:55 -0300

    v3

--- base-apparel-coming-soon-master/script.js
+++ base-apparel-coming-soon-master/script.js
@@ -1,11 +1,27 @@
 
-const email = document.getElementById('email')
-const button = document.getElementById('button')
+let email = document.getElementById('email')
+let form = document.getElementById('buttonText')
+let iconError = document.getElementById('iconError')
+let errorEmail = document.getElementById('errorEmail')
 
 
-    button.addEventListener('click', function(){
-        const valorInput = email.value
-        console.log(valorInput)
-    })   
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if(email.value === '' || validarEmail(email.value) !== true) {
+        email.style.borderColor = 'red'
+        iconError.style.display = 'block'
+        errorEmail.style.display = 'block'
+        errorEmail.textContent = 'Please provide a valid e-mail!'
+    } else {
+        iconError.style.display = 'none'
+        errorEmail.style.display = 'none'
+        email.style.borderColor = 'green'
+        console.log(email.value)
+    }
+})
 
 
+function validarEmail(email) { 
+    let emailtest = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    return emailtest.test(email)
+}
