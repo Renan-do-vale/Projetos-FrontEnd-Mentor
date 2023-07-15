c660d3cca42d917d6488e2728a024b7ea3184ccc
Renan do vale ferreira <Renandovaleferreira@gmail.com>
2023-07-15 13:30:21 -0300

    v1

---
+++ base-apparel-coming-soon-master/script.js
@@ -0,0 +1,11 @@
+
+const email = document.getElementById('email')
+const button = document.getElementById('button')
+
+
+    button.addEventListener('click', function(){
+        const valorInput = email.value
+        console.log(valorInput)
+    })   
+
+
